bde22376968f5e748e545bb3f8ccc3b187cea829
Hensel <henseljahja@gmail.com>
2021-02-24 10:50:43 +0700

    -

--- s.py
+++ s.py
@@ -1,23 +1,16 @@
-# t = int(input())
-# for _ in range(t):
-#     n = int(input())
-#     a = list(map(int, input().split()))
-#     if a[0] + a[1] > a[-1]:
-#         print(-1)
-#     else:
-#         print(1, 2, n)
-
-n = int(input())
-c = 1
-a = list(map(int, input().split()))
-m = 1
-for i in range(1,len(a)):
-    if  a[i] > a[i-1]:
-        c+=1
-    else:
-        if c > m:
-            m = c
-        c=1
-    if c > m:
-        m =c
-print(m)
+t = int(input())
+for i in range(t):
+    n = int(input())
+    x,y = 0,0
+    c=0
+    a = list(map(int, input().split()))
+    for i in range(len(a)):
+        if a[i] == 1:
+            x = i
+            break
+    for j in range(len(a)):
+        if a[-j] == 1:
+            y = j
+            break
+    c += a[x:-y].count(0)
+    print(c)
